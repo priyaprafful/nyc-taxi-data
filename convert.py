47229172be95da4be738cc70b01d7177d022fcb5
Priyanka Maheshwari <43381242+priyaprafful@users.noreply.github.com>
2026-04-03 10:37:52 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,1 +1,13 @@
 
+import pandas as pd
+
+# Load parquet file
+df = pd.read_parquet("yellow_tripdata_2026-01.parquet")
+
+# OPTIONAL: take a smaller sample (recommended)
+df = df.head(100)
+
+# Convert to CSV
+df.to_csv("yellow_trip_sample.csv", index=False)
+
+print("Conversion done!")
